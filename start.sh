ede4789b1af1bf733068be546c908534cfd3159d
cryptomcgrath <edward@edwardmcgrath.com>
2022-12-23 20:03:19 -0500

    change start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pm2 --name pyrestia-stat start "npm run start" -- start
+pm2 --name pyrestia-stat start "npm run start" -- start --restart-delay 10000
 sleep 7
-pm2 start ./thermostat.py 
+pm2 start ./thermostat.py --restart-delay 15000
 
