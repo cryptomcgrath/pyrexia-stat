dbdc6e20bd73f6abf947677f0dc4d63b136f73b4
cryptomcgrath <edward@edwardmcgrath.com>
2023-01-01 13:38:07 -0500

    auto gen token key if needed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,16 @@
 var db = require("../database.js")
 var createError = require("http-errors")
 const jwt = require("jsonwebtoken")
+var crypto = require("crypto")
 require("dotenv").config()
+
+// create TOKEN_KEY if not exists in .env file
+if (process.env.TOKEN_KEY == null) {
+    crypto.randomBytes(48, (err, buffer) => {
+        var token = buffer.toString('hex');
+        process.env.TOKEN_KEY = token
+    })
+}
 
 exports.checkNotAlreadyRegistered = (req, res, next) => {
   db.get("select * from user limit 1", (err, row) => {
